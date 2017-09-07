3a47cb6af4ab5fdb5fc2fa57d687e61b38a42db0
JosephTien <joseph811223@gmail.com>
2017-09-07 22:30:09 +0800

    ~

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
  */
 var linebot = require('linebot');
 var express = require('express');
-
 var bot = linebot({
   channelId: '1534193517',
   channelSecret: '396ce85776aac89c96ef32bf7de7ff57',
   channelAccessToken: '/i8orRNkILi4hQ4CzOqVzz4n6zva2hY3q+Z8viH+afCsVS/RkxMC733FW71NCa6Mf8Tteaqznt4EpzdxUtyI5jBg8B2haNaw6cCw5unWwQ3vSv8rifx+A+o0pnpBE94zyoG4l0pzxApivOz85iShFAdB04t89/1O/w1cDnyilFU='
 });
-/**************************************/
+main();
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -20,15 +19,18 @@ var server = app.listen(process.env.PORT || 8080, function() {
   console.log("App now running on port", port);
 });
 /**************************************/
-bot.on('message', function(event) {
-  if (event.message.type = 'text') {
-    var msg = '"'+event.message.text+'" is not a valid instruction';
-    event.reply(msg).then(function(data) {
-      // success 
-      console.log(msg);
-    }).catch(function(error) {
-      // error 
-      console.log('error');
-    });
-  }
-});
+function main(){
+  bot.on('message', function(event) {
+    if (event.message.type = 'text') {
+      var msg = '"'+event.message.text+'" is not a valid instruction';
+      event.reply(msg).then(function(data) {
+        // success 
+        console.log(msg);
+      }).catch(function(error) {
+        // error 
+        console.log('error');
+      });
+    }
+  });
+
+}
