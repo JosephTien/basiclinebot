01b8e44e529456dd817fe0ac8a440462860ddc90
JosephTien <joseph811223@gmail.com>
2017-09-07 22:10:14 +0800

    ~

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ var bot = linebot({
   channelSecret: '396ce85776aac89c96ef32bf7de7ff57',
   channelAccessToken: '/i8orRNkILi4hQ4CzOqVzz4n6zva2hY3q+Z8viH+afCsVS/RkxMC733FW71NCa6Mf8Tteaqznt4EpzdxUtyI5jBg8B2haNaw6cCw5unWwQ3vSv8rifx+A+o0pnpBE94zyoG4l0pzxApivOz85iShFAdB04t89/1O/w1cDnyilFU='
 });
-
-bot.on('message', function(event) {
-  console.log(event); //把收到訊息的 event 印出來看看
-});
-
+/**************************************/
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -22,4 +18,17 @@ app.post('/', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
-}); 
+});
+/**************************************/
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });
+  }
+});
